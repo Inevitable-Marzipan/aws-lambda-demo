681e1e813074cbe1564687cc1ca1b8825240194f
TimJ <tprjohnson1@gmail.com>
2022-05-18 20:26:12 +0100

    Adds lambda function and terraform

---
+++ main.tf
@@ -0,0 +1,61 @@
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "Spacelift_Test_Lambda_Function_Role"
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "iam_policy_lambda_logging_function"
+  path        = "/"
+  description = "AWS IAM Policy for logging from lambda"
+  policy      = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+data "archive_file" "python_file" {
+  type        = "zip"
+  source_dir  = "${path.module}/python/"
+  output_path = "${path.module}/python/hello-python.zip"
+}
+
+resource "aws_lambda_function" "lambda_func" {
+  filename      = "${path.module}/python/hello-python.zip"
+  function_name = "Test_Lambda_Function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.8"
+  depends_on    = [aws_iam_role_policy_attachment.policy_attach]
+}
