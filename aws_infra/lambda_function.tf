441d519a3b1960b9ae566ab1d93527a3296939ab
TimJ <tprjohnson1@gmail.com>
2022-07-24 22:17:19 +0100

    Adds lambda layer and requests to lambda function

--- aws_infra/lambda_function.tf
+++ aws_infra/lambda_function.tf
@@ -5,11 +5,12 @@ data "archive_file" "python_file" {
 }
 
 resource "aws_lambda_function" "lambda_func" {
-  filename      = data.archive_file.python_file.output_path
-  function_name = "Test_Lambda_Function"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.lambda_handler"
-  runtime       = "python3.8"
+  filename         = data.archive_file.python_file.output_path
+  function_name    = "Test_Lambda_Function"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.lambda_handler"
+  runtime          = "python3.8"
   source_code_hash = data.archive_file.python_file.output_base64sha256
-  depends_on    = [aws_iam_role_policy_attachment.policy_attach]
+  layers           = [aws_lambda_layer_version.lambda_layer.arn]
+  depends_on       = [aws_iam_role_policy_attachment.policy_attach]
 }
---
+++ aws_infra/lambda_layer.tf
@@ -0,0 +1,38 @@
+# Used as filename for the lambda layer zip
+resource "random_uuid" "lambda_src_hash" {
+  keepers = {
+    "${local.lambda_src_path}/requirements.txt" : filemd5("${local.lambda_src_path}/requirements.txt")
+  }
+}
+
+# Installs packages using pip
+resource "null_resource" "install_dependencies" {
+  provisioner "local-exec" {
+    command = "pip install -r ${local.lambda_src_path}/requirements.txt -t ${local.lambda_src_path}/dependencies/ --upgrade"
+  }
+
+  # Only re-run this if the dependencies or their versions
+  # have changed since the last deployment with Terraform
+  triggers = {
+    dependencies_versions = filemd5("${local.lambda_src_path}/requirements.txt")
+    # source_code_hash = random_uuid.lambda_src_hash.result # This is a suitable option too
+  }
+}
+
+data "archive_file" "lambda_layer_zip" {
+  type        = "zip"
+  source_dir  = "${local.lambda_src_path}/dependencies/"
+  output_path = "${path.module}/.tmp/${random_uuid.lambda_src_hash.result}.zip"
+
+  # This is necessary, since archive_file is now a
+  # `data` source and not a `resource` anymore.
+  # Use `depends_on` to wait for the "install dependencies"
+  # task to be completed.
+  depends_on = [null_resource.install_dependencies]
+}
+
+resource "aws_lambda_layer_version" "lambda_layer" {
+  filename   = data.archive_file.lambda_layer_zip.output_path
+  layer_name = "lambda_layer"
+
+}
