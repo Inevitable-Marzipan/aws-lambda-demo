44562391fe418bba1444f96092ce252a7373ec43
TimJ <tprjohnson1@gmail.com>
2022-07-24 22:11:14 +0100

    Refactors lambda function

--- aws_infra/lambda_function.tf
+++ aws_infra/lambda_function.tf
@@ -5,7 +5,7 @@ data "archive_file" "python_file" {
 }
 
 resource "aws_lambda_function" "lambda_func" {
-  filename      = "${path.module}/../python/hello-python.zip"
+  filename      = "${local.lambda_src_path}/lambda_function/hello-python.zip"
   function_name = "Test_Lambda_Function"
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
