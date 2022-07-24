4e4fbabf09d3eec6581d70a1002f53dbdc7651bc
TimJ <tprjohnson1@gmail.com>
2022-07-25 00:09:38 +0100

    Edits script

--- src/build_layer.sh
+++ src/build_layer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export PKG_DIR="dependencies2"
+cd src
 rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
 
 docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.8 pip3 install -r requirements.txt --no-deps -t ${PKG_DIR}
