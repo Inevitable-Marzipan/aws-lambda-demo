873d38f6e2559159a32c37db8735ff52285c0acd
TimJ <tprjohnson1@gmail.com>
2022-07-24 22:52:16 +0100

    Changes handler name

--- aws_infra/lambda_function.tf
+++ aws_infra/lambda_function.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "lambda_func" {
   filename         = data.archive_file.python_file.output_path
   function_name    = "Test_Lambda_Function"
   role             = aws_iam_role.lambda_role.arn
-  handler          = "index.lambda_handler"
+  handler          = "hello-python.lambda_handler"
   runtime          = "python3.8"
   source_code_hash = data.archive_file.python_file.output_base64sha256
   layers           = [aws_lambda_layer_version.lambda_layer.arn]
