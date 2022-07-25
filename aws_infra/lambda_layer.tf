1a6e31128cbda329580492e81e1f000f1762d3eb
TimJ <tprjohnson1@gmail.com>
2022-07-25 01:31:03 +0100

    Tidies up dependencies

--- aws_infra/lambda_layer.tf
+++ aws_infra/lambda_layer.tf
@@ -8,7 +8,7 @@ resource "random_uuid" "lambda_src_hash" {
 # Installs packages using pip
 resource "null_resource" "install_dependencies" {
   provisioner "local-exec" {
-    command = "pip install -r ${local.lambda_src_path}/requirements.txt -t ${local.lambda_src_path}/dependencies/ --upgrade"
+    command = "sh ${local.lambda_src_path}/build_layer.sh"
   }
 
   # Only re-run this if the dependencies or their versions
@@ -20,27 +20,16 @@ resource "null_resource" "install_dependencies" {
   }
 }
 
-  # Installs packages using pip
-resource "null_resource" "install_dependencies2" {
-  provisioner "local-exec" {
-    command = "sh ${local.lambda_src_path}/build_layer.sh"
-  }
-
-  triggers = {
-    always_run = "${timestamp()}"
-  }
-}
-
 data "archive_file" "lambda_layer_zip" {
   type        = "zip"
-  source_dir  = "${local.lambda_src_path}/dependencies2/"
+  source_dir  = "${local.lambda_src_path}/dependencies/"
   output_path = "${path.module}/.tmp/${random_uuid.lambda_src_hash.result}.zip"
 
   # This is necessary, since archive_file is now a
   # `data` source and not a `resource` anymore.
   # Use `depends_on` to wait for the "install dependencies"
   # task to be completed.
-  depends_on = [null_resource.install_dependencies2]
+  depends_on = [null_resource.install_dependencies]
 }
 
 resource "aws_lambda_layer_version" "lambda_layer" {
--- src/build_layer.sh
+++ src/build_layer.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-export PKG_DIR="dependencies2/python/lib/python3.8/site-packages/"
+export PKG_DIR="dependencies/python/lib/python3.8/site-packages/"
 cd src
 rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
 
 docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.8 pip3 install -r requirements.txt --no-deps -t ${PKG_DIR}
-
-echo $(ls) 
-cd dependencies2
-echo $(ls)
