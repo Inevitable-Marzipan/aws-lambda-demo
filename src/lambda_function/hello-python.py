cabaeb29bb660a6e1bec138a8d3cb56ae0159802
TimJ <tprjohnson1@gmail.com>
2022-07-25 23:51:32 +0100

    tests parameter store

--- src/lambda_function/hello-python.py
+++ src/lambda_function/hello-python.py
@@ -1,8 +1,12 @@
 import requests
+import boto3
+
+ssm = boto3.client('ssm')
+parameter = ssm.get_parameter(Name='username', WithDecryption=True)['Parameter']['Value']
 
 def lambda_handler(event, context):
 
-   message = 'Hello {} !'.format(event['key1'])
+   message = 'Hello {} !, username: {}'.format(event['key1'], parameter)
    return {
        'message' : message
    }
