3f1aba8f00c44777d074badc2e770960caa5dc49
TimJ <tprjohnson1@gmail.com>
2022-08-10 22:19:05 +0100

    saves data out to s3

--- src/lambda_function/lambda_function.py
+++ src/lambda_function/lambda_function.py
@@ -1,6 +1,13 @@
+import os
+import json
 import time
 import datetime
 import requests
+import boto3
+
+url = ""
+auth = ('username', 'password')
+
 
 def get_unix_start_time(date):
     return int(time.mktime(date.timetuple()))
@@ -10,14 +17,15 @@ def get_unix_end_time(date):
     return get_unix_start_time(date)
 
 def get_data(url, auth=None, params=None):
-    resp = requests.get(url)
+    resp = requests.get(url, auth=auth, params=params)
     data = resp.json()
 
     return data
 
 def lambda_handler(event, context):
-
-    data = None
-    return {
-        'data' : data
-    }
+    unix_start = get_unix_start_time(datetime.datetime.strptime(event['time'], '%Y-%m-%dT%H:%M:%SZ'))
+    unix_end = get_unix_end_time(datetime.datetime.strptime(event['time'], '%Y-%m-%dT%H:%M:%SZ'))
+    params = {'begin': unix_start, 'end': unix_end}
+    data = get_data(url, auth=auth, params=params)
+    client = boto3.client('s3', region_name=os.environ['AWS_REGION'])
+    client.put_object(Body=json.dumps(data), Bucket=os.environ['bucket'], Key='test.json')
--- src/tests/test_lambda_function.py
+++ src/tests/test_lambda_function.py
@@ -1,16 +1,22 @@
-from unittest import mock
 from lambda_function.lambda_function import (
+    url,
+    auth,
      get_unix_start_time,
      get_unix_end_time,
      get_data,
      lambda_handler
 )
+from tests.conftest import MockResponse
 
+import os
 import datetime
 import json
 from unittest.mock import patch
+from unittest import mock
 
-from tests.conftest import MockResponse
+from moto import mock_s3
+import boto3
+import pytest
 
 def test_get_unix_start_time():
     date = datetime.date(2022, 1, 1)
@@ -30,21 +36,43 @@ def test_get_unix_end_time():
 
 def test_get_data():
     url = ""
-    expected_data = {'data': 'data'}
+    auth = ('username', 'password')
+    params = {"key": "value"}
+    expected_data = {'data_key': 'data_value'}
     status_code = 200
     with patch('lambda_function.lambda_function.requests') as mock_requests:
         mock_requests.get.return_value = MockResponse(expected_data, status_code)
-        actual = get_data(url)
+        actual = get_data(url, auth, params)
     
         assert actual == expected_data
         mock_requests.get.assert_called_once()
-        mock_requests.get.assert_called_with(url)
+        mock_requests.get.assert_called_with(url, auth=auth, params=params)    
 
+@mock_s3
+@mock.patch.dict(os.environ, {"bucket": "test_bucket", "AWS_REGION": "eu-west-2"})
 def test_lambda_handler():
-    event = None
+    bucket_name = os.environ['bucket']
+    region = os.environ['AWS_REGION']
+    conn = boto3.resource('s3', region)
+    conn.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': region})
+
+    event = {"time": "2015-10-08T16:53:06Z"}
     context = None
-    expected = {"data": None}
 
-    actual = lambda_handler(event, context)
+    unix_start = get_unix_start_time(datetime.datetime.strptime(event['time'], '%Y-%m-%dT%H:%M:%SZ'))
+    unix_end = get_unix_end_time(datetime.datetime.strptime(event['time'], '%Y-%m-%dT%H:%M:%SZ'))
+    params = {'begin': unix_start, 'end': unix_end}
+    json_data = {"data_key": "data_value"}
+    status_code = 200
 
-    assert actual == expected
+    with patch('lambda_function.lambda_function.get_data') as mock_get_data:
+        mock_get_data.return_value = json_data
+
+        lambda_handler(event, context)
+        mock_get_data.assert_called_once()
+        mock_get_data.assert_called_with(url, auth=auth, params=params)
+
+    body = conn.Object(bucket_name, 'test.json').get()[
+    'Body'].read().decode("utf-8")
+
+    assert json.loads(body) == json_data
