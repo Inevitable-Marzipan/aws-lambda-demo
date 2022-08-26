0d6368a50120d8b312d954e961b97f372e74f40d
TimJ <tprjohnson1@gmail.com>
2022-08-26 21:29:59 +0100

    Changes resource

--- aws_infra/s3.tf
+++ aws_infra/s3.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "config_bucket" {
   bucket = "openskynetwork-config-bucket"
 }
 
-resource "aws_s3_bucket_object" "config_object" {
+resource "aws_s3_object" "config_object" {
   bucket = aws_s3_bucket.config_bucket.id
   key    = "step_function.json"
   source = "../config/step_function.json"
