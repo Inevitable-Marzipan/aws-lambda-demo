c16a308ed8ab888e854e9b9b6eb5d4a29d8465dc
Tim Johnson <43890311+Inevitable-Marzipan@users.noreply.github.com>
2022-08-26 21:31:32 +0100

    Adds config and uploads to s3 bucket (#9)
    * Adds config and uploads to s3 bucket

--- aws_infra/s3.tf
+++ aws_infra/s3.tf
@@ -1,3 +1,15 @@
 resource "aws_s3_bucket" "data_bucket" {
   bucket = "openskynetwork-bucket"
 }
+
+resource "aws_s3_bucket" "config_bucket" {
+  bucket = "openskynetwork-config-bucket"
+}
+
+resource "aws_s3_object" "config_object" {
+  bucket = aws_s3_bucket.config_bucket.id
+  key    = "step_function.json"
+  source = "../config/step_function.json"
+
+  etag = filemd5("${path.module}/../config/step_function.json")
+}
