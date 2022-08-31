d274d3103826e48a5886c9318caf5365fa2ca232
TimJ <tprjohnson1@gmail.com>
2022-08-31 23:51:05 +0100

    Update cw event rule cron

--- aws_infra/cloudwatch.tf
+++ aws_infra/cloudwatch.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "sfn_cron_rule" {
   name                = "TriggerStepFunction"
   description         = "Cron schedule to trigger Step Funtion"
-  schedule_expression = "cron(0 22 * * ? *)"
+  schedule_expression = "cron(59 23 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "sfn" {
