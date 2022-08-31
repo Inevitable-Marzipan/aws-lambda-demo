47d82686ad7b40e4bd43ec080209258f67f34e62
TimJ <tprjohnson1@gmail.com>
2022-08-31 22:50:00 +0100

    Change sfn config parsing

--- aws_infra/sfn.tf
+++ aws_infra/sfn.tf
@@ -3,27 +3,26 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
   role_arn = aws_iam_role.step_function_role.arn
 
   definition = <<EOF
-
 {
   "StartAt": "GetObject",
   "States": {
     "GetObject": {
       "Type": "Task",
       "Parameters": {
-        "Bucket": "${aws_s3_bucket.config_bucket.id}",
-        "Key": "${aws_s3_object.config_object.id}"
+        "Bucket": "openskynetwork-config-bucket",
+        "Key": "step_function.json"
       },
       "Resource": "arn:aws:states:::aws-sdk:s3:getObject",
-      "End": true,
       "ResultSelector": {
-        "myJson.$": "States.StringToJson($.Body)"
-      }
+        "airplane_icao24s.$": "States.StringToJson($.Body)"
+      },
+      "End": true,
+      "ResultPath": "$.config"
     }
   },
   "Comment": "S3 -> JSON",
   "TimeoutSeconds": 60
 }
-
 EOF
 
   depends_on = [aws_iam_role_policy_attachment.policy_attach_step_function]
