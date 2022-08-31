ac2a85f779bf1a6a65271a030255cd65171dbef7
TimJ <tprjohnson1@gmail.com>
2022-08-31 22:03:36 +0100

    Adds role arn

---
+++ aws_infra/cloudwatch.tf
@@ -0,0 +1,12 @@
+resource "aws_cloudwatch_event_rule" "sfn_cron_rule" {
+  name                = "TriggerStepFunction"
+  description         = "Cron schedule to trigger Step Funtion"
+  schedule_expression = "cron(0 22 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "sfn" {
+  rule      = aws_cloudwatch_event_rule.sfn_cron_rule.name
+  target_id = "TriggerSfn"
+  arn       = aws_sfn_state_machine.sfn_state_machine.arn
+  role_arn  = aws_iam_role.cw_event_role.arn
+}
