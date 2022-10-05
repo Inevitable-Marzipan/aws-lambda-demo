cd091f4ea0708eed57bf8f394cc2a4843a6ec675
TimJ <tprjohnson1@gmail.com>
2022-10-05 21:35:17 +0100

    Adds logging to date offsetter

--- src/lambda_functions/date_offsetter.py
+++ src/lambda_functions/date_offsetter.py
@@ -1,10 +1,20 @@
 import datetime
+import logging
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)  # To see output in local console
+logger.setLevel(logging.INFO)  # To see output in Lambda
 
 def lambda_handler(event, context):
 
+    logger.info(f"event: {event}")
+    logger.info(f"context: {context}")
+
     if event.get('offset', None) is None:
+        logger.info("No offset key given, exiting")
         return event
 
+    logger.info("Calculating date offset")
     input_datetime = datetime.datetime.strptime(event['time'], '%Y-%m-%dT%H:%M:%SZ')
     td = datetime.timedelta(days=event.get('offset').get('days'))
     output_datetime = input_datetime + td
