da5b6365b11d6f608d6f5efafe1a7e0245ffaaf0
TimJ <tprjohnson1@gmail.com>
2022-10-05 21:06:48 +0100

    Adds tf for lambda function

--- aws_infra/lambda_function.tf
+++ aws_infra/lambda_function.tf
@@ -1,16 +1,16 @@
-data "archive_file" "python_file" {
+data "archive_file" "opensky_network_scraper_file" {
   type        = "zip"
   source_file  = "${local.lambda_src_path}/lambda_functions/opensky_network_scraper.py"
   output_path = "${local.lambda_src_path}/opensky_network_scraper.zip"
 }
 
 resource "aws_lambda_function" "lambda_func" {
-  filename         = data.archive_file.python_file.output_path
+  filename         = data.archive_file.opensky_network_scraper_file.output_path
   function_name    = "OpenSkyNetworkScraper"
   role             = aws_iam_role.lambda_role.arn
   handler          = "opensky_network_scraper.lambda_handler"
   runtime          = "python3.8"
-  source_code_hash = data.archive_file.python_file.output_base64sha256
+  source_code_hash = data.archive_file.opensky_network_scraper_file.output_base64sha256
   layers           = [aws_lambda_layer_version.lambda_layer.arn]
   depends_on       = [aws_iam_role_policy_attachment.policy_attach_lambda]
   timeout          = 20
@@ -21,4 +21,21 @@ resource "aws_lambda_function" "lambda_func" {
     }
 
   }
+}
+
+data "archive_file" "date_offsetter_file" {
+  type        = "zip"
+  source_file  = "${local.lambda_src_path}/lambda_functions/date_offsetter.py"
+  output_path = "${local.lambda_src_path}/date_offsetter.zip"
+}
+
+resource "aws_lambda_function" "date_offsetter_lambda_func" {
+  filename         = data.archive_file.date_offsetter_file.output_path
+  function_name    = "DateOffsetter"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "date_offsetter.lambda_handler"
+  runtime          = "python3.8"
+  source_code_hash = data.archive_file.date_offsetter_file.output_base64sha256
+  depends_on       = [aws_iam_role_policy_attachment.policy_attach_lambda]
+
 }
